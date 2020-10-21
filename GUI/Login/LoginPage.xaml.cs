c74066459676ed2d890844c1d6c25dda057f3552
Sara Ho <70639189+sarathaoho@users.noreply.github.com>
2020-10-21 14:27:21 +0200

    Add files via upload

---
+++ GUI/Login/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using GUI.Home;
+using Logic.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.Login
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        private const string _errorMsg = "Inloggningen misslyckades";
+
+        private LoginService _loginService;
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            _loginService = new LoginService();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = this.tbUsernam.Text;
+            string password = this.pbPassword.Password;
+
+            bool successful = _loginService.Login(username, password);
+
+            if (successful)
+            {
+
+                HomePage homePage = new HomePage();
+
+                this.NavigationService.Navigate(homePage);
+            }
+            else
+            {
+
+                MessageBox.Show(_errorMsg);
+                this.tbUsernam.Clear();
+                this.pbPassword.Clear();
+            }
+        }
+    }
+}
---
+++ Logic/DAL/UserDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Logic.DAL
+{
+    public class UserDataAccess
+    {
+        private const string path = @"DAL\User.json";
+
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+        /// </summary>
+        /// <returns></returns>
+        public List<User> GetUsers()
+        {
+
+            string jsonString = File.ReadAllText(path);
+            List<User> users = JsonSerializer.Deserialize<List<User>>(jsonString);
+
+            return users;
+        }
+    }
+}
