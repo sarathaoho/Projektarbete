8c05afe0814eecb1696abf33931b47e4b04cd7f0
monophonik <70640207+monophonik@users.noreply.github.com>
2020-10-23 15:54:38 +0200

    interface
    Med IUser

--- Logic/Entities/Admin.cs
+++ Logic/Entities/Admin.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Logic.Entities
 {
-    public class Admin : User
+    public class Admin : User, IUser
     {
 
         private Admin()
--- Logic/Entities/User.cs
+++ Logic/Entities/User.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Logic.Entities
 {
-    public class User
+    public class User : IUser
     {
         public string Username { get; set; }
         public string Password { get; set; }
