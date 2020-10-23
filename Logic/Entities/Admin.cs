fec7b6e8276a7508b7df3aae9ff4832b32699080
monophonik <70640207+monophonik@users.noreply.github.com>
2020-10-23 08:00:37 +0200

    Adminklass
    Admin

---
+++ Logic/Entities/Admin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Entities
+{
+    public class Admin : User //???
+    {
+
+        public void AddMechanic()
+        {
+
+        }
+
+
+        public void ChangeMechanic()
+        {
+
+        }
+
+        public void AddMechanicSkill()
+        {
+
+        }
+
+        public void RemoveMechanicSkill()
+        {
+
+        }
+
+        public void AddUser() //Måste kopplas till en mekaniker.
+        {
+
+        }
+
+        public void RemoveUser()
+        {
+
+        }
+
+        public void AddErrand()
+        {
+
+        }
+
+    }
+}
