8ff45bebcbcd410a052c32bee088128ca770f40e
monophonik <70640207+monophonik@users.noreply.github.com>
2020-10-28 10:29:57 +0100

    asdfasdf

---
+++ Logic/Services/AdamService.cs
@@ -0,0 +1,19 @@
+﻿using Logic.DAL;
+using Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Services
+{
+    public static class AdamService
+    {
+
+        public static void AddUser(UserDB user)
+        {
+            DataAccess<UserDB> dataAccess = new DataAccess<UserDB>();
+
+            dataAccess.AddEntity(user);
+        }
+    }
+}
